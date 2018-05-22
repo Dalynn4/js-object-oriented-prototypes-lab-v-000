61e59ea17ee64fffcb76845d35b057084cfdfd33
Randall Robinson <keith_adams007@yahoo.com>
2018-05-22 23:09:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,9 +3,7 @@
    this.homeState = homeState
    this.training = training
 
-   this.veto = function(){
-     return 'No, I must disagree'
-   }
+   this
    this.approve = function(){
      return 'You can do that!'
    }
