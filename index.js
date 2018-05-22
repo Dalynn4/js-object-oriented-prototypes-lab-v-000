7c6d41dd90ea866fce1ac29c07a30e5ba558e45a
Randall Robinson <keith_adams007@yahoo.com>
2018-05-22 23:10:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,5 +26,9 @@ BoardMember.prototype.doCharity = function(){
      return "I like to help people."
    }
    
+BoardMember.prototype.releasePressStatement = function(){
+     return "You will see great things from Scuber."
+   }
+   
 BoardMember.prototype   
 
